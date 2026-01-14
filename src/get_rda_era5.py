371277cd335e97bac547c7d3754a4279b84e9f62
Miles Epstein <miles.epstein@comcast.net>
2026-01-14 10:53:15 -0700

    accessed era5 on glade and built pipeline, ran model on small data

--- src/get_rda_era5.py
+++ src/get_rda_era5.py
@@ -12,35 +12,24 @@ import concurrent.futures
 LEVEL_DATA_PATH = "/glade/campaign/collections/rda/data/d633000/e5.oper.an.pl/"
 SFC_DATA_PATH = "/glade/campaign/collections/rda/data/d633000/e5.oper.an.sfc/"
 
-var_to_file_name = {
-    "t2m":"2t",
-    "u10m":"10u",
-    "v10m":"10v",
-    "v100m":"100v",
-    "u100m":"100u"
-} 
 
 CHANNEL_TO_CODE = {
-    "z": 129, # z200
+    "10u": 165,
+    "10v": 166,
+    "2t": 167,
+    "2d": 168,
+    "z": 129,
+    "tisr": 212,
+
+    "pv": 60,
+    "q": 133,
+    "t": 130,
     "u": 131,
     "v": 132,
-    "t": 130,
-    "q": 133,
-    "r": 157,
-    "t2m": 167,
-    "u10m": 165,
-    "v10m": 166,
-    # "u100m": 228246,
-    # "v100m": 228247,
-    "u100m": 246,
-    "v100m": 247,
-
-    "tcwv": 137,
-    "sp": 134,
-    "msl": 151,
-    # total precip
-    "tp": 228,
+    "w": 135
 }
+
+SURFACE_CHANNELS = ["10u", "10v", "2t", "2d", "z", "tisr"]
 
 
 @dataclasses.dataclass
@@ -58,7 +47,7 @@ class SingleLevelCode:
 
 
 def process_code(code, SFC_DATA_PATH, LEVEL_DATA_PATH, year, month, month_end_day, day, time):
-    if code.name in ['u','v']:
+    if code.name in ['u', 'v']:
         termll025 = 'll025uv'
     else:
         termll025 = 'll025sc'
@@ -83,7 +72,7 @@ def process_code(code, SFC_DATA_PATH, LEVEL_DATA_PATH, year, month, month_end_da
     elif isinstance(code, PressureLevelCode):
         dataarray = path_data[var_name].loc[{"time": time, "level": code.level}].drop_vars("level").expand_dims({"channel": 1})
 
-    dataarray = dataarray.rename({"latitude": "lat", "longitude": "lon"})
+    # dataarray = dataarray.rename({"latitude": "lat", "longitude": "lon"})
     return dataarray
 
 def open_casper_nc(codes, time):
@@ -91,7 +80,6 @@ def open_casper_nc(codes, time):
     year = str(time.year)
     month = str(time.month).zfill(2)
     day = str(time.day).zfill(2)
-    hour = time.hour
     month_end_date = time + relativedelta(day=31)
     month_end_day = month_end_date.day
     # Main part
@@ -104,30 +92,24 @@ def open_casper_nc(codes, time):
 
     dataarray_ls = [future.result() for future in dataarray_futures]
     dataarray_ls = xr.concat(dataarray_ls, dim="channel")
-    return dataarray_ls    
-    
+    return dataarray_ls
+
+
 def parse_channel(channel: str) -> Union[PressureLevelCode, SingleLevelCode]:
-    if channel in list(var_to_file_name.keys()): 
-        name = var_to_file_name[channel] 
-    else :
-        name = channel
-    if channel in CHANNEL_TO_CODE:
-        if channel in ['u100m','v100m','u10n','v10n','tcsw','tcrw','ltlt','lshf','lict','tp']:
-            return SingleLevelCode(CHANNEL_TO_CODE[channel], name = name,code0=228)
-        else:
-            return SingleLevelCode(CHANNEL_TO_CODE[channel], name = name)
+    if channel in SURFACE_CHANNELS:
+        return SingleLevelCode(CHANNEL_TO_CODE[channel], name = channel)
     else:
-        code = CHANNEL_TO_CODE[channel[0]]
-        name = name[0]
-        level = int(channel[1:])
-        return PressureLevelCode(code, name=name, level=int(level))
+        code = CHANNEL_TO_CODE[channel[:-3]]
+        level = int(channel[-3:])
+        return PressureLevelCode(code, name=channel[:-3], level=int(level))
+
 
 def _get_channels(time: datetime.datetime, channels: List[str]):
     codes = [parse_channel(c) for c in channels]
     # darray = _download_codes(client, codes, time)
     darray = open_casper_nc(codes, time)
     return (darray.assign_coords(channel=channels).assign_coords(time=time).expand_dims("time").transpose(
-        "time", "channel", "lat", "lon")
+        "time", "channel", "latitude", "longitude")
             # .assign_coords(lon=darray["lon"] + 180.0)
             # .roll(lon=1440 // 2)
            )
@@ -157,7 +139,7 @@ if __name__ == "__main__":
         'u700', 'u600', 'u500', 'u400', 'u300', 'u250', 'u200', 'u150', 'u100', 'u50', 'v1000', 'v925', 'v850', 'v700',
         'v600', 'v500', 'v400', 'v300', 'v250', 'v200', 'v150', 'v100', 'v50', 'msl', 'u10m', 'v10m', 't2m' #
     ]
-    channel0 = ['tp'] #['t850', 'z1000', 'z700', 'z500', 'z300', 'tcwv', 't2m','tp']
+    channel0 = ['tp']  # ['t850', 'z1000', 'z700', 'z500', 'z300', 'tcwv', 't2m','tp']
     # for name in pangu_channel[-10:]:
     #     print(parse_channel(name))
     # for name in pangu_channel[:3]:
